f4fb72cd0696564d096a7c7700542c2fd260a89e
thatipallyvinodkumar167 <thatipallyvinodkumar167@gmail.com>
2026-03-19 18:28:31 +0530

    Allow Vercel origin in CORS policy

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -4,7 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5001;
 export const MONGO_URI = process.env.MONGO_URI;
-export const CLIENT_ORIGINS = [
-    "http://localhost:3000",
-    "https://daycatchadmin.vercel.app"
-];
+export const CLIENT_ORIGINS = process.env.CLIENT_ORIGINS 
+    ? process.env.CLIENT_ORIGINS.split(",") 
+    : ["http://localhost:3000", "https://daycatchadmin.vercel.app"];
